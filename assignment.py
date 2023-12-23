57c23bbe8e77ec0f74cb8674a9dd12ce1c22ea7a
Nick Hardman <kp22738@bristol.ac.uk>
2023-12-23 16:18:38 +0000

    sql table created

--- assignment.py
+++ assignment.py
@@ -1,2 +1,12 @@
-#testing commit
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
 
+
+database_service = "sqlite"
+database = "jupiter.db"
+connectable = f"{database_service}:///{database}"
+
+query="SELECT distance_km FROM moons"
+
+pd.read_sql(query, connectable)
