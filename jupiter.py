a6bcea03d111db7818fe4fda06f9365cc075b0f2
Nick Hardman <kp22738@bristol.ac.uk>
2023-12-23 21:20:19 +0000

    added a function in the moon class which creates an sql query which creates a table for the moon data

--- jupiter.py
+++ jupiter.py
@@ -7,3 +7,8 @@ class Moons:
         database_service = "sqlite"
         self.connectable = f"{database_service}:///{db}"
         self.load_data()
+    
+    def load_data(self):
+       
+        query = "SELECT * FROM moons"
+        self.data = pd.read_sql(query, self.connectable)
